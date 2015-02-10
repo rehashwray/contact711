1d98d871a8160f5cb3b314046f5d3541ed9f150b
marvin <mpc6388@gmail.com>
2015-02-10 12:10:34 -0400

    test credential

--- contact/src/main/java/db/DbCredentials.java
+++ contact/src/main/java/db/DbCredentials.java
@@ -1,14 +1,5 @@
 package db;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-import logger.Log;
-
-import com.google.common.base.Charsets;
-import com.google.common.io.Files;
-
 public class DbCredentials {
 
 	private static String url;
@@ -21,18 +12,7 @@ public class DbCredentials {
 	
 	private static void initialize() {
 		
-		File file = new File("DbCredentialsFile.txt");
-		List<String> lines = null;
-		try {
-			lines = Files.readLines(file, Charsets.UTF_8);
-		} catch (IOException e) {
-			Log.logError(e);
-			return;
-		}
 
-		url = lines.get(0);
-		user = lines.get(1);
-		password = lines.get(2);
 	}
 
 	public static String getUrl() {
